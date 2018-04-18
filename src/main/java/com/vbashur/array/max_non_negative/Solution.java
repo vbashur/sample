ebff0b063c7b51820c1f02541a53082abc0d725f
vbashur <vbashur@yandex.ru>
2018-04-18 17:26:38 +0200

    array subseq

--- src/main/java/com/vbashur/array/max_non_negative/Solution.java
+++ src/main/java/com/vbashur/array/max_non_negative/Solution.java
@@ -16,58 +16,54 @@ public class Solution {
     private static final Integer START_INDEX_DEFAULT_VALUE = -1;
 
     public ArrayList<Integer> maxset(ArrayList<Integer> A) {
-        Map<Integer, ArrayList<Integer>> subarrMap = new HashMap();
-        Integer startIndex = START_INDEX_DEFAULT_VALUE;
-        for (int iter = 0; iter < A.size(); ++iter) {
-            if (isNonNegative(A.get(iter))) {
-                if (startIndex == START_INDEX_DEFAULT_VALUE) {
-                    startIndex = iter;
-                }
-            } else if (isNonNegative(startIndex)) {
-                subarrMap.put(startIndex, new ArrayList<>(A.subList(startIndex, iter)));
-                startIndex = START_INDEX_DEFAULT_VALUE;
-            }
-        }
-
-        Map<Integer, Pair<Integer, Integer>> sumsSegentsMap = new HashMap<Integer, Pair<Integer, Integer>>();
-        for (Integer ind : subarrMap.keySet()) {
-            List<Integer> segment = subarrMap.get(ind);
-            Pair<Integer, Integer> sumSegm = new Pair<Integer, Integer>(segment.stream().mapToInt(Integer::intValue).sum(),
-                    segment.size());
-            sumsSegentsMap.put(ind, sumSegm);
-        }
-        Integer MAX_SUM = Integer.MIN_VALUE;
-        Integer MAX_LENGTH = Integer.MIN_VALUE;
-        Integer MIN_INDEX = Integer.MAX_VALUE;
-        for (Integer ind : sumsSegentsMap.keySet()) {
-            Pair<Integer, Integer> sumSegm = sumsSegentsMap.get(ind);
-            if (sumSegm.getKey() > MAX_SUM) {
-                MAX_SUM = sumSegm.getKey();
-                MAX_LENGTH = sumSegm.getValue();
-                MIN_INDEX = ind;
-            }
-            if (sumSegm.getKey() == MAX_SUM) {
-                if (sumSegm.getValue() > MAX_LENGTH) {
-                    MAX_LENGTH = sumSegm.getValue();
-                    MIN_INDEX = ind;
-                }
-                if (sumSegm.getValue() == MAX_LENGTH) {
-                    if (ind < MIN_INDEX) {
-                        MIN_INDEX = ind;
+            long maxSum = 0;
+            long maxSeq = 0;
+            ArrayList<Integer> res = new ArrayList<>();
+            long seq = 0, sum = 0;
+            int index = -1;
+            for (int iter = 0; iter < A.size(); ++iter) {
+                int curVal = A.get(iter);
+                if (isNonNegative(curVal)) {
+                    if (!isNonNegative(index)) {
+                        index = iter;
                     }
+                    sum += curVal;
+                    seq +=1;
+                } else {
+                    if (sum > maxSum) {
+                        res.clear();
+                        res.addAll(A.subList(index, iter));
+                        maxSum = sum;
+                    }
+                    if (sum == maxSum && sum >= 0) {
+                        if (seq > maxSeq) {
+                            res.clear();
+                            res.addAll(A.subList(index, iter));
+                            maxSeq = seq;
+                        }
+                    }
+                    sum = 0;
+                    index = -1;
+                    seq = 0;
                 }
             }
-        }
-        if (MIN_INDEX < Integer.MAX_VALUE) {
-            return subarrMap.get(MIN_INDEX);
-        } else {
-            return null;
+            if (sum > maxSum) {
+                res.clear();
+                res.addAll(A.subList(index, A.size()));
+                maxSum = sum;
+            }
+            if (sum == maxSum && sum >= 0) {
+                if (seq > maxSeq) {
+                    res.clear();
+                    res.addAll(A.subList(index, A.size()));
+                }
+            }
+            return res;
+
         }
 
-    }
-
-    private static boolean isNonNegative(int num) {
-        return num > -1;
-    }
+        private static boolean isNonNegative(long num) {
+            return num > -1;
+        }
 
 }
