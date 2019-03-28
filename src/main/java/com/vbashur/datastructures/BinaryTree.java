b948c680a027a7e7f0b44ee42ed8a5dfe0a56558
vbashur <vbashur@yandex.ru>
2019-03-28 09:39:49 +0100

    Trees traversals

--- src/main/java/com/vbashur/datastructures/BinaryTree.java
+++ src/main/java/com/vbashur/datastructures/BinaryTree.java
@@ -1,5 +1,7 @@
 package com.vbashur.datastructures;
 
+import java.util.LinkedList;
+
 import com.vbashur.common.TreeNode;
 
 public class BinaryTree {
@@ -34,20 +36,109 @@ public class BinaryTree {
         return sb.toString();
     }
 
-    public static String bfsRecursiveTraversal( TreeNode root ) {
-        return "";
-
+    public static String bfsRecursiveTraversal( TreeNode root, StringBuilder sb ) {
+        if( root != null ) {
+            System.out.println( root );
+            sb.append( "<" + root.val + "> " );
+            bfsRecursiveTraversalHelper( root, sb );
+        }
+        return sb.toString();
     }
 
-    public static String bfsIterativeTraversal( TreeNode root ) {
-        return "";
+    private static String bfsRecursiveTraversalHelper( TreeNode root, StringBuilder sb ) {
+        LinkedList<TreeNode> adjs = new LinkedList<>();
+        if( root.left != null ) adjs.add( root.left );
+        if( root.right != null ) adjs.add( root.right );
+        for( TreeNode tn : adjs ) {
+            System.out.println( tn );
+            sb.append( "<" + tn.val + "> " );
+
+        }
+        for( TreeNode tn : adjs ) {
+            bfsRecursiveTraversalHelper( tn, sb );
+        }
+        return sb.toString();
     }
 
-    public static String dfsRecursiveTraversal( TreeNode root ) {
-        return "";
+//    public static String bfsRecursiveTraversal( TreeNode root, StringBuilder sb ) {
+//        if( root != null ) {
+//            LinkedList<TreeNode> adjs = new LinkedList<>();
+//            if( root.left != null ) adjs.add( root.left );
+//            if( root.right != null ) adjs.add( root.right );
+//            bfsRecursiveTraversalHelper( root, sb, new HashSet<Integer>(), adjs );
+//        }
+//        return sb.toString();
+//    }
+//
+//    private static String bfsRecursiveTraversalHelper( TreeNode root, StringBuilder sb, Set<Integer> marked, LinkedList<TreeNode> adjs ) {
+//        if( !marked.contains( root.val ) ) {
+//            System.out.println( root );
+//            sb.append( "<" + root.val + "> " );
+//            marked.add( root.val );
+//        }
+//        for( TreeNode tn : adjs ) {
+//            if( !marked.contains( tn ) ) {
+//                System.out.println( tn );
+//                sb.append( "<" + tn.val + "> " );
+//                marked.add( tn.val );
+//            }
+//        }
+//        if( root.left != null ) {
+//            LinkedList<TreeNode> adjsLeft = new LinkedList<>();
+//            if( root.left.left != null ) adjsLeft.add( root.left.left );
+//            if( root.left.right != null ) adjsLeft.add( root.left.right );
+//            bfsRecursiveTraversalHelper( root.left, sb, marked, adjsLeft );
+//        }
+//        if( root.right != null ) {
+//            LinkedList<TreeNode> adjsRight = new LinkedList<>();
+//            if( root.right.left != null ) adjsRight.add( root.right.left );
+//            if( root.right.right != null ) adjsRight.add( root.right.right );
+//            bfsRecursiveTraversalHelper( root.right, sb, marked, adjsRight );
+//        }
+//        return sb.toString();
+//    }
+
+    public static String bfsIterativeTraversal( TreeNode root, StringBuilder sb ) {
+        if( root != null ) {
+            LinkedList<TreeNode> nodes = new LinkedList<>();
+            nodes.add( root );
+            while( !nodes.isEmpty() ) {
+                TreeNode node = nodes.pollFirst();
+                System.out.println( node );
+                sb.append( "<" + node.val + "> " );
+                if( node.left != null ) nodes.add( node.left );
+                if( node.right != null ) nodes.add( node.right );
+            }
+        }
+        return sb.toString();
     }
 
-    public static String dfsIterativeTraversal( TreeNode root ) {
-        return "";
+    public static String dfsRecursiveTraversal( TreeNode root, StringBuilder sb ) {
+        if( root != null ) {
+            if( root.left != null ) {
+                dfsRecursiveTraversal( root.left, sb );
+            }
+            if( root.right != null ) {
+                dfsRecursiveTraversal( root.right, sb );
+            }
+            System.out.println( root );
+            sb.append( "<" + root.val + "> " );
+        }
+        return sb.toString();
+    }
+
+    public static String dfsIterativeTraversal( TreeNode root, StringBuilder sb ) {
+        if( root != null ) {
+            LinkedList<TreeNode> nodes = new LinkedList<>();
+            nodes.add( root );
+            while( !nodes.isEmpty() ) {
+                TreeNode node = nodes.pollLast();
+                if( node.left != null ) nodes.add( node.left );
+                if( node.right != null ) nodes.add( node.right );
+                System.out.println( node );
+                sb.append( "<" + node.val + "> " );
+            }
+        }
+        return sb.toString();
     }
 }
--- src/test/java/com/vbashur/datastructures/BinaryTreeTest.java
+++ src/test/java/com/vbashur/datastructures/BinaryTreeTest.java
@@ -1,7 +1,5 @@
 package com.vbashur.datastructures;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,60 +7,67 @@ import com.vbashur.common.TreeNode;
 
 public class BinaryTreeTest {
 
-
-
     TreeNode root;
 
-        @Before
-        public void setup() {
-            root = new TreeNode( 1 );
-            TreeNode node2 = new TreeNode( 2 );
-            TreeNode node3 = new TreeNode( 3 );
-            TreeNode node4 = new TreeNode( 4 );
-            TreeNode node5 = new TreeNode( 5 );
-            TreeNode node6 = new TreeNode( 6 );
-            TreeNode node7 = new TreeNode( 7 );
-            root.left = node2;
-            root.right = node3;
-            node2.left = node4;
-            node2.right = node5;
-            node3.left = node6;
-            node3.right = node7;
-        }
+    @Before
+    public void setup() {
+        root = new TreeNode( 1 );
+        TreeNode node2 = new TreeNode( 2 );
+        TreeNode node3 = new TreeNode( 3 );
+        TreeNode node4 = new TreeNode( 4 );
+        TreeNode node5 = new TreeNode( 5 );
+        TreeNode node6 = new TreeNode( 6 );
+        TreeNode node7 = new TreeNode( 7 );
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+    }
+
     @Test
     public void preOrderTraversal() {
 
 
-        String treePrint = BinaryTree.preOrderTraversal( root, new StringBuilder(  ) );
-        System.out.println(treePrint);
+        String treePrint = BinaryTree.preOrderTraversal( root, new StringBuilder() );
+        System.out.println( treePrint );
 
     }
 
     @Test
     public void inOrderTraversal() {
-        String treePrint = BinaryTree.inOrderTraversal( root, new StringBuilder(  ) );
-        System.out.println(treePrint);
+        String treePrint = BinaryTree.inOrderTraversal( root, new StringBuilder() );
+        System.out.println( treePrint );
     }
 
     @Test
     public void postOrderTraversal() {
-        String treePrint = BinaryTree.postOrderTraversal( root, new StringBuilder(  ) );
-        System.out.println(treePrint);
+        String treePrint = BinaryTree.postOrderTraversal( root, new StringBuilder() );
+        System.out.println( treePrint );
     }
 
     @Test
     public void bfsRecursiveTraversal() {
+        String treePrint = BinaryTree.bfsRecursiveTraversal( root, new StringBuilder() );
+        System.out.println( treePrint );
     }
 
     @Test
     public void bfsIterativeTraversal() {
+        String treePrint = BinaryTree.bfsIterativeTraversal( root, new StringBuilder() );
+        System.out.println( treePrint );
     }
 
     @Test
     public void dfsRecursiveTraversal() {
+        String treePrint = BinaryTree.dfsRecursiveTraversal( root, new StringBuilder() );
+        System.out.println( treePrint );
     }
 
     @Test
     public void dfsIterativeTraversal() {
+        String treePrint = BinaryTree.dfsIterativeTraversal( root, new StringBuilder() );
+        System.out.println( treePrint );
     }
 }
