f467dc5df104f137b7890d35982ad84f807e0fa1
vbashur <vbashur@yandex.ru>
2019-03-29 12:46:17 +0100

    boxes stack - improved

--- src/main/java/com/vbashur/backtracking/stackboxes/Solution.java
+++ src/main/java/com/vbashur/backtracking/stackboxes/Solution.java
@@ -2,11 +2,10 @@ package com.vbashur.backtracking.stackboxes;
 
 
 /*
-how many boxes could be placed one in another - givw a maximum number
+how many boxes could be placed one in another - give a maximum number
  */
 
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 
 /*
@@ -29,62 +28,15 @@ public class Solution {
         }
 
         public boolean less( Box other ) {
-            List<int[]> box1Dims = new LinkedList<>();
-            int[] box1DimArray = new int[] { this.d, this.h, this.w };
-            doPerm( box1Dims, box1DimArray, box1DimArray.length );
-            printPerms(box1Dims);
-            List<int[]> box2Dims = new LinkedList<>();
-            int[] box2DimArray = new int[] { other.d, other.h, other.w };
-            doPerm( box2Dims, box2DimArray, box2DimArray.length );
-            printPerms(box2Dims);
-            for( int[] box1Arr : box1Dims ) {
-                for( int[] box2Arr : box2Dims ) {
-                    boolean isLess = true;
-                    for( int iter = 0; iter < box1Arr.length && isLess; ++iter ) {
-                        if( box1Arr[iter] < box2Arr[iter] ) {
-                            isLess = false;
-                        }
-                    }
-                    if( isLess ) return true;
-                }
+
+            int[] thisBox = new int[] { this.d, this.h, this.w };
+            int[] otherBox = new int[] { other.d, other.h, other.w };
+            for( int i = 0; i < thisBox.length; ++i ) {
+                if( thisBox[i] >= otherBox[i] ) return false;
             }
-            return false;
-
+            return true;
         }
-
-        public void doPerm( List<int[]> dimList, int[] dimensions, int index ) {
-
-            if( index <= 0 )
-                dimList.add( dimensions );
-            else { //recursively solve this by placing all other items at current first pos
-                doPerm( dimList, dimensions, index - 1 );
-                int currPos = dimensions.length - index;
-                for( int i = currPos + 1; i < dimensions.length; i++ ) {//start swapping all other items with current first item
-                    swap( dimensions, currPos, i );
-                    doPerm( dimList, dimensions, index - 1 );
-                    swap( dimensions, i, currPos );//restore back
-                }
-            }
-        }
-
-        private void swap( int[] input, int a, int b ) {
-            int tmp = input[a];
-            input[a] = input[b];
-            input[b] = tmp;
-        }
-
-        private void printPerms(List<int[]> boxDims) {
-            System.out.println( "print permutations:" );
-            for (int[] dims : boxDims) {
-                for (int i = 0; i < dims.length; ++i ) {
-                    System.out.print(dims[i] + " " );
-                }
-                System.out.println( );
-            }
-        }
-
     }
-
 
     public int getStackedBoxes( List<Box> boxes ) {
         boxes.sort( boxComparator );
@@ -94,9 +46,10 @@ public class Solution {
         }
         for( int iter = 0; iter < boxes.size() - 1; ++iter ) {
             int counter = 1;
-            while( boxes.get( iter ).less( boxes.get( iter + 1 ) ) ) {
+            while( (iter < boxes.size() - 1) && boxes.get( iter ).less( boxes.get( iter + 1 ) ) ) {
                 ++counter;
                 if( counter > maxSequence ) maxSequence = counter;
+                ++iter;
             }
         }
         return maxSequence;
@@ -106,59 +59,7 @@ public class Solution {
     public Comparator<Box> boxComparator = new Comparator<Box>() {
         @Override
         public int compare( Box o1, Box o2 ) {
-            return o2.less( o1 ) ? 1 : -1;
+            return o1.less( o2 ) ? -1 : 1;
         }
     };
-
-
-//    public void doPerm(List<int[]> dimList, int[] dimensions, int index){
-//
-//        if(index <= 0)
-//            dimList.add(dimensions);
-//        else { //recursively solve this by placing all other chars at current first pos
-//            doPerm(dimList, dimensions, index-1);
-//            int currPos = dimensions.length-index;
-//            for (int i = currPos+1; i < dimensions.length; i++) {//start swapping all other chars with current first char
-//                swap(dimensions,currPos, i);
-//                doPerm(dimList, dimensions, index-1);
-//                swap(dimensions,i, currPos);//restore back my string buffer
-//            }
-//        }
-//    }
-//
-
-//    public int[] getBoxDimensions(Box box) {
-//        int[] indexes = new int[]{box.w, box.h, box.d};
-//        int i = 0;
-//        while (i < indexes.length) {
-//            if (indexes[i] < i) {
-//                swap(indexes, i % 2 == 0 ?  0: indexes[i], i);
-//                indexes[i]++;
-//                i = 0;
-//            }
-//            else {
-//                indexes[i] = 0;
-//                i++;
-//            }
-//        }
-//    }
-//
-//    public void shuffleAllRecursive( int n, int[] elements) {
-//
-//        if(n == 1) {
-//            return;
-//        } else {
-//            for(int i = 0; i < n-1; i++) {
-//                shuffleAllRecursive(n - 1, elements);
-//                if(n % 2 == 0) {
-//                    swap(elements, i, n-1);
-//                } else {
-//                    swap(elements, 0, n-1);
-//                }
-//            }
-//            shuffleAllRecursive(n - 1, elements);
-//        }
-//    }
-
-
 }
--- src/test/java/com/vbashur/backtracking/stackboxes/SolutionTest.java
+++ src/test/java/com/vbashur/backtracking/stackboxes/SolutionTest.java
@@ -2,17 +2,33 @@ package com.vbashur.backtracking.stackboxes;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
 
+    Solution s = new Solution();
     @Test
     public void getStackedBoxes() {
 
         Solution.Box b1 = new Solution.Box( 1, 2, 3 );
         Solution.Box b2 = new Solution.Box( 2, 3, 4 );
         Assert.assertTrue( b1.less( b2 ) );
+
+        int res = s.getStackedBoxes( Arrays.asList(b1, b2) );
+        Assert.assertEquals( 2, res );
     }
 
+    @Test
+    public void getStackedBoxesComplex() {
+        Solution.Box b1 = new Solution.Box( 1, 2, 3 );
+        Solution.Box b2 = new Solution.Box( 2, 3, 4 );
+        Solution.Box b3 = new Solution.Box( 5, 3, 4 );
+        Solution.Box b4 = new Solution.Box( 4, 4, 7 );
+        Solution.Box b5 = new Solution.Box( 3, 9, 2 );
+        int res = s.getStackedBoxes( Arrays.asList(b1, b2, b3, b4, b5) );
+        Assert.assertEquals( 2, res );
+    }
 }
