57dfb9d7444b08c163909f2f1d98febdbb23d619
vbashur <vbashur@yandex.ru>
2019-06-13 17:14:18 +0200

    Second attempt on Python: solution for leet 997. Find the Town Judge

--- pysrc/leet/findtownjudge/solution.py
+++ pysrc/leet/findtownjudge/solution.py
@@ -1,30 +1,27 @@
-
-
 class Solution(object):
     def findJudge(self, N, trust):
         parents = [-1 for i in range(0, N + 1)]
 
         for pair in trust:
-            parents[pair[0]] = pair[1]
+            if (parents[pair[0]] == -1):
+                parents[pair[0]] = [pair[1]]
+            else:
+                parents[pair[0]].append(pair[1])
 
         judge = -1
         for i in range(1, len(parents)):
-            print(parents[i])
+            if (parents[i] == (-1)):
+                if (judge > -1):
+                    return -1
+                else:
+                    judge = i
 
+        for i in range(1, len(parents)):
+            if (i != judge and not (self.contains_judge(parents[i], judge))):
+                return -1
+        return judge
 
-            if (parents[i] == -1 and judge == -1) :
-                # print(str(parents[i]) + "-1-" + str(judge))
-                judge = parents[i]
-            elif (parents[i] != -1 and judge != -1) :
-                # print(str(parents[i]) + "<2>" + str(judge))
-                if (parents[i] != judge) :
-                    judge = 0
-            elif (parents[i] != -1 and judge == -1):
-                # print(str(parents[i]) + "<3>" + str(judge))
-                judge = parents[i]
-            elif (parents[i] == -1 and judge != i):
-                # print(str(parents[i]) + "<4>" + str(judge))
-                judge = 0
-        print("hui")
-        print(judge)
-        return judge if (judge > 0) else -1
+    def contains_judge(self, arr, judge):
+        for item in arr:
+            if item == judge: return True
+        return False
--- pysrc/leet/findtownjudge/test_solution.py
+++ pysrc/leet/findtownjudge/test_solution.py
@@ -6,11 +6,19 @@ class TestSolution(TestCase):
 
     def test_findJudge(self):
         s = solution.Solution()
-        # res = s.findJudge(3, [[1,2],[2,3],[1,3]])
-        # self.assertEqual(3, res, "positive test")
-        #
-        # res = s.findJudge(3, [[1, 2], [2, 3],])
-        # self.assertEqual(-1, res, "negative test")
+        res = s.findJudge(3, [[1, 2], [2, 3], [1, 3]])
+        self.assertEqual(3, res, "positive test")
+
+        res = s.findJudge(3, [[1, 2], [2, 3], ])
+        self.assertEqual(-1, res, "negative test")
+
         res = s.findJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]])
         self.assertEqual(3, res, "positive test")
+
+        res = s.findJudge(4, [[1, 2], [2, 1], [3, 4]])
+        self.assertEqual(-1, res, "negative test")
+
+        res = s.findJudge(4, [[1, 4], [2, 4], [3, 4], [1, 2]])
+        self.assertEqual(4, res, "negative test")
+
         print("cool!")
