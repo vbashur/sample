34bc24138e16e5ef035e10dc90207e82ba1993b7
vbashur <vbashur@yandex.ru>
2019-09-26 17:11:43 +0200

    LC medium: consequent 7

--- src/main/java/com/vbashur/graph/numswithsameconsecdiff/Solution.java
+++ src/main/java/com/vbashur/graph/numswithsameconsecdiff/Solution.java
@@ -1,6 +1,10 @@
 package com.vbashur.graph.numswithsameconsecdiff;
 
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Added on 24/09/19
  * https://leetcode.com/problems/numbers-with-same-consecutive-differences/
@@ -8,12 +12,45 @@ package com.vbashur.graph.numswithsameconsecdiff;
 public class Solution {
 
     public int[] numsSameConsecDiff(int N, int K) {
+        int[] digits = new int[] {1,2,3,4,5,6,7,8,9};
         if (N < 2) {
             if (K == 0) {
-                return new int[] {1,2,3,4,5,6,7,8,9};
+                return digits;
             }
             return new int[0];
         }
-        return new int[0];
+        List<int[]> nums = new LinkedList<>();
+        for (int i = 0; i < digits.length; ++i) {
+            int newNum = K - i;
+            if (newNum >= 0) {
+                int[] arr = new int[]{i, K-i};
+                System.out.print(arr[0]);
+                System.out.println(arr[1]);
+                nums.add(new int[]{i, K-i});
+            }
+        }
+
+        return getRes(nums, N);
+    }
+
+    private int[] getRes(List<int[]> nums, int length) {
+        int limit = (int)Math.pow(10, length) - 1;
+        int[] res = new int[nums.size()];
+        for (int i = 0; i < res.length; ++i) {
+            res[i] = 0;
+        }
+        int counter = 1;
+        while (counter <= length) {
+            for (int i = 0 ; i < nums.size(); ++i) {
+                int[] arr = nums.get(i);
+                int curNum = (int)Math.pow(10, counter - 1) * arr[arr.length - counter];
+                res[i] = res[i] + curNum;
+                System.out.print(res[i]);
+                System.out.println(":"+curNum);
+            }
+            ++counter;
+        }
+        return res;
+
     }
 }
---
+++ src/test/java/com/vbashur/graph/numswithsameconsecdiff/SolutionTest.java
@@ -0,0 +1,22 @@
+package com.vbashur.graph.numswithsameconsecdiff;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void numsSameConsecDiff() {
+        Solution s = new Solution();
+        int[] arr = s.numsSameConsecDiff(2, 7);
+//        for (int i =0; i < arr.length; ++i) {
+//            System.out.println(arr[i]);
+//        }
+//
+//        Stream.of(s.numsSameConsecDiff(2, 7)).forEach(System.out::println);
+    }
+}
