e1222757de11aef483e05b72e8cc4543d4b45e2e
vbashur <vbashur@yandex.ru>
2019-09-26 22:18:24 +0200

    added: omg still cannot solve diff conseq

--- src/test/java/com/vbashur/graph/numswithsameconsecdiff/SolutionTest.java
+++ src/test/java/com/vbashur/graph/numswithsameconsecdiff/SolutionTest.java
@@ -12,10 +12,11 @@ public class SolutionTest {
     @Test
     public void numsSameConsecDiff() {
         Solution s = new Solution();
-        int[] arr = s.numsSameConsecDiff(2, 7);
-//        for (int i =0; i < arr.length; ++i) {
-//            System.out.println(arr[i]);
-//        }
+        int[] arr = s.numsSameConsecDiff(3, 2);
+        System.out.println("__");
+        for (int i =0; i < arr.length; ++i) {
+            System.out.println(arr[i]);
+        }
 //
 //        Stream.of(s.numsSameConsecDiff(2, 7)).forEach(System.out::println);
     }
