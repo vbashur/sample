70dc1d884b10ada0ae494c82694ba0c687672275
vic <v.bashurov@carfax.eu>
2019-12-17 08:37:38 +0100

    LC network delay time placeholder

---
+++ src/main/java/com/vbashur/graph/network_delay_time/Solution.java
@@ -0,0 +1,28 @@
+package com.vbashur.graph.network_delay_time;
+
+/*
+https://leetcode.com/problems/network-delay-time/
+ */
+public class Solution {
+
+    /*
+
+    There are N network nodes, labelled 1 to N.
+
+    Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node, v is the target node,
+    and w is the time it takes for a signal to travel from source to target.
+
+    Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal? If it is impossible, return -1.
+
+
+
+    Example 1:
+
+    Input: times = [[2,1,1],[2,3,1],[3,4,1]], N = 4, K = 2
+    Output: 2
+
+     */
+    public int networkDelayTime(int[][] times, int N, int K) {
+        return -1;
+    }
+}
