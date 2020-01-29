de0120f6a77d28eb20a02470e0f1e394658236bc
vbashur <vbashur@yandex.ru>
2020-01-29 22:49:26 +0100

    network delay: wrong attempt - TODO debug

--- src/main/java/com/vbashur/graph/network_delay_time/Solution.java
+++ src/main/java/com/vbashur/graph/network_delay_time/Solution.java
@@ -1,5 +1,11 @@
 package com.vbashur.graph.network_delay_time;
 
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /*
 https://leetcode.com/problems/network-delay-time/
  */
@@ -23,6 +29,50 @@ public class Solution {
 
      */
     public int networkDelayTime(int[][] times, int N, int K) {
-        return -1;
+
+        Map<Integer, List<int[]>> graphMap = new HashMap<>();
+
+        for (int iter = 0; iter < times.length; ++iter) {
+
+            if (graphMap.containsKey(times[iter][0])) {
+                graphMap.get(times[iter][0]).add(new int[]{times[iter][1], times[iter][2]});
+            } else {
+                List<int[]> list = new LinkedList();
+                list.add(new int[]{times[iter][1], times[iter][2]});
+                graphMap.put(times[iter][0], list);
+            }
+
+        }
+        Map<Integer, Integer> routes = new HashMap<>();
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.addFirst(K);
+        routes.put(K, 0);
+        while (!queue.isEmpty()) {
+            int nodeId = queue.pollFirst();
+            if (graphMap.containsKey(nodeId)) {
+                for (int[] nodeRoute : graphMap.get(nodeId)) {
+                    int key = nodeRoute[0];
+                    int routeValue = nodeRoute[1] + routes.get(nodeId);
+                    if (routes.containsKey(key)) {
+                        int newRouteValue = routes.get(key) + routes.get(nodeId);
+                        routes.put(key, Math.min(routeValue, newRouteValue));
+                    } else {
+                        routes.put(key, nodeRoute[1]);
+                        queue.addLast(nodeRoute[0]);
+                    }
+                }
+            }
+
+        }
+        if (routes.size() != N) {
+            return -1;
+        }
+        int max = Integer.MIN_VALUE;
+        for (Integer key : routes.keySet()) {
+            max = Math.max(routes.get(key), max);
+        }
+        return max;
+
+
     }
 }
