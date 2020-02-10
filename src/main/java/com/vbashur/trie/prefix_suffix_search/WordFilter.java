701841348426486abad87afd300cf0910853d56a
vbashur <vbashur@yandex.ru>
2020-02-11 00:01:16 +0100

    LC hard prefix & suffix search

--- src/main/java/com/vbashur/trie/prefix_suffix_search/WordFilter.java
+++ src/main/java/com/vbashur/trie/prefix_suffix_search/WordFilter.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+/*
+08/02/2020
+https://leetcode.com/problems/prefix-and-suffix-search/
+ */
 public class WordFilter {
 
     public Trie prefixes = new Trie();
@@ -21,13 +25,13 @@ public class WordFilter {
                     curr.indices.offer(iter);
                     curr = newTrie;
                 } else {
+                    curr.indices.offer(iter);
                     curr = curr.values.get(symb);
                 }
 
             }
         }
 
-//        Trie postfixes = new Trie();
         for (int iter = 0; iter < words.length; ++iter) {
             Trie curr = postfixes;
             for (int i = words[iter].length() - 1; i >= 0; --i) {
@@ -38,61 +42,72 @@ public class WordFilter {
                     curr.indices.offer(iter);
                     curr = newTrie;
                 } else {
+                    curr.indices.offer(iter);
                     curr = curr.values.get(symb);
                 }
             }
         }
 
-
-        /*
-        for (int iter = 0; iter < words.length; ++iter ){
-            Map<String, Map> currMap = prefixes;
-            for (int i = 0; i < words[iter].length(); ++i) {
-                String symb = words[iter].substring(i, i+1);
-                if (!currMap.containsKey(symb)) {
-                    Map newMap = new HashMap<>();
-                    currMap.put(symb, newMap);
-                    currMap = newMap;
-                } else {
-                    currMap = currMap.get(symb);
-                }
-            }
-        }
-
-        for (int iter = 0; iter < words.length; ++iter ){
-            Map<String, Map> currMap = postfixes;
-            for (int i = words[iter].length() - 1; i >= 0 ; --i) {
-                String symb = words[iter].substring(i, i+1);
-                if (!currMap.containsKey(symb)) {
-                    Map newMap = new HashMap<>();
-                    currMap.put(symb, newMap);
-                    currMap = newMap;
-                } else {
-                    currMap = currMap.get(symb);
-                }
-            }
-        }*/
-
-
     }
 
     public int f(String prefix, String suffix) {
-        return 2;
+        suffix = getReverted(suffix);
+        PriorityQueue<Integer> prefPQ = getWeights(prefix, prefixes);
+        PriorityQueue<Integer> suffPQ = getWeights(suffix, postfixes);
+        if (prefPQ != null && suffPQ != null) {
+            while (!prefPQ.isEmpty()
+             && !suffPQ.isEmpty()) {
+                int prefPeek = prefPQ.peek();
+                int suffPeak = suffPQ.peek();
+                if (suffPeak == prefPeek) {
+                    return suffPeak;
+                }
+                if (suffPeak > prefPeek) {
+                    suffPQ.poll();
+                } else {
+                    prefPQ.poll();
+                }
+            }
 
+        }
+        return -1;
     }
 
-    private class Trie {
+    private String getReverted(String suffix) {
+        char[] reverted = new char[suffix.length()];
+        for (int iter = suffix.length() - 1; iter >= 0; --iter) {
+            reverted[suffix.length() - 1 - iter] = suffix.charAt(iter);
+        }
+        return new String(reverted);
+    }
+
+    PriorityQueue<Integer> getWeights(String word, Trie tmpPrefixes) {
+        for (int iter = 0; iter < word.length(); ++iter) {
+            String symb = word.substring(iter, iter + 1);
+            if (tmpPrefixes.values.containsKey(symb)) {
+                if (iter == word.length() - 1) {
+                    return tmpPrefixes.values.get(symb).indices;
+                }
+                tmpPrefixes = tmpPrefixes.values.get(symb);
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    private static class Trie {
         public PriorityQueue<Integer> indices = new PriorityQueue<>((e1, e2) -> e2.compareTo(e1));
         //        public String key;
         public Map<String, Trie> values = new HashMap<>();
 
-        @Override
-        public String toString() {
-            return "Trie{" +
-                    "indices=" + indices +
-                    ", values=" + values +
-                    '}';
-        }
+//        @Override
+//        public String toString() {
+//            return "Trie{" +
+//                    "indices=" + indices +
+//                    "\n\t, values=" + values +
+//                    '}';
+//        }
     }
 }
 
@@ -101,4 +116,6 @@ public class WordFilter {
  * WordFilter obj = new WordFilter(words);
  * int param_1 = obj.f(prefix,suffix);
  */
+
+
 
--- src/test/java/com/vbashur/trie/prefix_suffix_search/WordFilterTest.java
+++ src/test/java/com/vbashur/trie/prefix_suffix_search/WordFilterTest.java
@@ -9,10 +9,14 @@ public class WordFilterTest {
 
     @Test
     public void test() {
-        String[] testString = new String[] {"abc", "def", "apple"};
+        String[] testString = new String[] {"abacabac", "def", "ape","apple", "abacklgjlaubac"};
+//        String[] testString = new String[] {"ab"};
         WordFilter wf = new WordFilter(testString);
+
         System.out.println(wf.prefixes);
         System.out.println(wf.postfixes);
+//        System.out.println(wf.f("a", "e"));
+        System.out.println(wf.f("abac", "abac"));
     }
 
 }
