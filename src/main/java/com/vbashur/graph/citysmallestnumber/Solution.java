9cf19a21298abe1a9655b0d63cd800e11bd43388
vbashur <vbashur@yandex.ru>
2020-02-19 22:16:58 +0100

    LC MED smallest city: proper solution but TLE

--- src/main/java/com/vbashur/graph/citysmallestnumber/Solution.java
+++ src/main/java/com/vbashur/graph/citysmallestnumber/Solution.java
@@ -10,13 +10,13 @@ https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbor
 public class Solution {
 
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        Map<Integer, List<int[]>> citiesMap = getCitiesMap(edges);
+        Map<Integer, List<int[]>> citiesMap = getCitiesMap(edges, n);
         int resCityId = -1;
         int maxNumCities = Integer.MAX_VALUE;
 
-        for (Integer city : citiesMap.keySet()) {
-            int numReachableCities = getReachableCities(city, citiesMap, distanceThreshold);
-            if (numReachableCities <= maxNumCities && numReachableCities > 0) {
+        for (int city = 0; city < n; ++city) {
+            int numReachableCities = getReachableCities(city, citiesMap, distanceThreshold, n);
+            if (numReachableCities <= maxNumCities) {
                 maxNumCities = numReachableCities;
                 resCityId = city;
             }
@@ -26,60 +26,47 @@ public class Solution {
     }
 
 
-    private Map<Integer, List<int[]>> getCitiesMap(int[][] edges) {
+    private Map<Integer, List<int[]>> getCitiesMap(int[][] edges, int n) {
         Map<Integer, List<int[]>> citiesMap = new HashMap<>();
+        for (int iter = 0; iter < n; ++iter) {
+            citiesMap.put(iter, new LinkedList<int[]>());
+        }
         for (int iter = 0; iter < edges.length; ++iter) {
-            if (!citiesMap.containsKey(edges[iter][0])) {
-                List<int[]> list = new LinkedList<int[]>();
-                list.add(new int[]{edges[iter][1], edges[iter][2]});
-                citiesMap.put(edges[iter][0], list);
-            } else {
-                citiesMap.get(edges[iter][0]).add(new int[]{edges[iter][1], edges[iter][2]});
-            }
-
-            if (!citiesMap.containsKey(edges[iter][1])) {
-                List<int[]> list = new LinkedList<int[]>();
-                list.add(new int[]{edges[iter][0], edges[iter][2]});
-                citiesMap.put(edges[iter][1], list);
-            } else {
-                citiesMap.get(edges[iter][1]).add(new int[]{edges[iter][0], edges[iter][2]});
-            }
+            citiesMap.get(edges[iter][0]).add(new int[]{edges[iter][1], edges[iter][2]});
+            citiesMap.get(edges[iter][1]).add(new int[]{edges[iter][0], edges[iter][2]});
         }
         return citiesMap;
     }
 
-    private int getReachableCities(Integer city, Map<Integer, List<int[]>> citiesMap, int distanceThreshold) {
+    private int getReachableCities(Integer city, Map<Integer, List<int[]>> citiesMap, int distanceThreshold, int n) {
+        Map<Integer, Integer> weights = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            if (i == city.intValue()) {
+                weights.put(i, 0);
+            } else {
+                weights.put(i, Integer.MAX_VALUE);
+            }
+        }
+        helper2(city, weights, citiesMap);
+        int num = 0;
+        for (Integer w : weights.values()) {
+            if ( w <= distanceThreshold ) {
+                ++num;
+            }
+        }
+        return num;
+    }
+
+    private void helper2(Integer city, Map<Integer, Integer> weights, Map<Integer, List<int[]>> citiesMap) {
         List<int[]> dests = citiesMap.get(city);
-        int num = 0;
-        for (int[] dest : dests) {
-            if (dest[1] <= distanceThreshold) {
-                ++num;
-                Set<Integer> markedCities = new HashSet<>();
-                markedCities.add(city);
-                markedCities.add(dest[1]);
-                if (distanceThreshold - dest[1] > 0) {
-                    num += helper(markedCities, dest[0], citiesMap, distanceThreshold - dest[1]);
-                }
+        int cityWeight = weights.get(city);
+        for (int[] d : dests) {
+            int targetCity = d[0];
+            int targetWeight = d[1];
+            if (cityWeight + targetWeight < weights.get(targetCity)) {
+                weights.put(targetCity, cityWeight + targetWeight);
+                helper2(targetCity, weights, citiesMap);
             }
         }
-        return num;
     }
-
-    private int helper(Set<Integer> markedCities, Integer source, Map<Integer, List<int[]>> citiesMap, int threshold) {
-        int num = 0;
-        for (int[] dest : citiesMap.get(source)) {
-            if (!markedCities.contains(dest[0]) && dest[1] <= threshold) {
-                ++num;
-                markedCities.add(source);
-                if (threshold - dest[1] > 0) {
-                    num += helper(markedCities, dest[0], citiesMap, threshold - dest[1]);
-                }
-            }
-        }
-        return num;
-
-
-    }
-
-
 }
