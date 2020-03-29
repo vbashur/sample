e9b02d927fc2fe3f9d9a5bae04e5d66de0028344
vic <v.bashurov@carfax.eu>
2020-03-30 00:07:25 +0200

    amaz 2nd edition

--- src/main/java/com/vbashur/amaz/SolutionOn30302020.java
+++ src/main/java/com/vbashur/amaz/SolutionOn30302020.java
@@ -3,10 +3,14 @@ package com.vbashur.amaz;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /**
  * Test ID: 23280666274486
@@ -25,22 +29,32 @@ public class SolutionOn30302020 {
 
         for(String fr : featureRequests) {
             String[] words = fr.split(" ");
+            Set<String> set = new HashSet<>();
+
             for (int iter = 0; iter < words.length; ++iter) {
-                if (countMap.containsKey(words[iter])) {
-                    Integer currCounter = countMap.get(words[iter]);
-                    countMap.put(words[iter], currCounter + 1);
-                }
+                set.add(words[iter]);
             }
+            set.forEach(w -> {
+                if (countMap.containsKey(w)) {
+                    Integer currCounter = countMap.get(w);
+                    countMap.put(w, currCounter + 1);
+                }
+            });
+
+
         }
-        PriorityQueue<StrIntPair> pq = new PriorityQueue<>();
+        LinkedList<StrIntPair> pq = new LinkedList<>();
 
         for (String key : countMap.keySet()) {
             pq.add(new StrIntPair(key, countMap.get(key)));
         }
 
+        pq.sort((pair1, pair2) -> pair2.counter.compareTo(pair1.counter));
+        new ArrayList<String>().addAll(pq.subList(0, 3).stream().map(p->p.key).collect(Collectors.toList()));
+
         ArrayList<String> popularNFeatures = new ArrayList<>(numFeatureRequests);
         for (int iter = 0; iter < numFeatureRequests; ++iter) {
-            popularNFeatures.add(pq.poll().key);
+            popularNFeatures.add(pq.pollFirst().key);
         }
 
         return popularNFeatures;
